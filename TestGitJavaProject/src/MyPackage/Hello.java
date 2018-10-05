8884be6d43574c2cd703dd7a017e25cf491515f9
Peter Wang <Peter.Wang@dserve.net>
2018-10-05 12:20:48 -0700

    test commit

--- TestGitJavaProject/src/MyPackage/Hello.java
+++ TestGitJavaProject/src/MyPackage/Hello.java
@@ -3,6 +3,6 @@ package MyPackage;
 public class Hello {
    public static void main(String[] args)
    {
-	   System.out.println("hello 123");
+	   System.out.println("hello adjfasdkjfhaskd");
    }
 }
