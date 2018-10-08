225dd300d8b4a5e9414aeb478d5367955c8a0007
Peter Wang <Peter.Wang@dserve.net>
2018-10-08 09:42:26 -0700

    test

--- SecondProject/src/Package1/Test.java
+++ SecondProject/src/Package1/Test.java
@@ -3,6 +3,10 @@ package Package1;
 public class Test {
   public static void main(String[] args)
   {
+<<<<<<< master
     System.out.println("hello git 908 12321");
+=======
+    System.out.println("hello git 134");
+>>>>>>> be5897f Update Test.java
   }
 }
